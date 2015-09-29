20d8bf53f09471260bc49cf9262c659d0edd2b9d
Roman Sutormin <rsutormin@lbl.gov>
2015-09-29 10:36:55 -0700

    Forgot to add one file

---
+++ scripts/run_async.sh
@@ -0,0 +1,5 @@
+script_dir=$(dirname "$(readlink -f "$0")")
+export PERL5LIB=$script_dir/../lib:$PERL5LIB
+export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
+WORK_DIR=/kb/deployment/services/contigcount/work
+cat $WORK_DIR/token | xargs perl $script_dir/../lib/Bio/KBase/contigcount/Server.pm $WORK_DIR/input.json $WORK_DIR/output.json
